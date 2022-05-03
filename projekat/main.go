4812d9f6e4c54c52261639fc919831ab07181bd9
jovana112 <jovanamilisic1000@yahoo.com>
2022-05-03 19:03:55 +0100

    homework added

--- projekat/main.go
+++ projekat/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+
+	server := Servis{
+		Data: map[string][]*Config{},
+	}
+	router.HandleFunc("/configs", server.createConfigHandler).Methods("POST")
+	router.HandleFunc("/configs", server.getAllHandler).Methods("GET")
+	router.HandleFunc("/configs/{id}", server.getConfigHandler).Methods("GET")
+	router.HandleFunc("/configs/{id}", server.deleteConfigHandler).Methods("DELETE")
+	router.HandleFunc("/configs/{id}", server.extendConfigGroupHandler).Methods("PATCH")
+
+	// start server
+	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: router}
+	go func() {
+		log.Println("server starting")
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	<-quit
+
+	log.Println("service shutting down ...")
+
+	// gracefully stop server
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server stopped")
 }
